d534be050daf098f90b1273a465d00a1f2440799
Bala Luguja <bluguja@gmail.com>
2020-05-11 13:20:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,19 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     
+    page = Nokogiri::HTML(open(index_url))
+    students = []
+
+    page.css("div.student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name,
+                :location => location,
+                :profile_url => profile_url}
+        end
+      students << student_info
+      
   end
 
   def self.scrape_profile_page(profile_url)
